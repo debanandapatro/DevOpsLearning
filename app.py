5a994753b83283025242084054798cc909d77d09
Debananda <Debananda@gmail.com>
2021-09-26 16:03:43 +0000

    Commting from Master for conflict

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 print("Hello World")
 print("Second Line")
 print("This line is added from dev branch")
+print "this is from master for understanding conflict"
 print("This is another change in dev branch")
 
